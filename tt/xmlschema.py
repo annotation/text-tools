224b23654f45d2abd54e358709618e30332d75e0
Dirk Roorda <dirk.roorda@di.huc.knaw.nl>
2023-02-13 21:51:36 +0100

    updates

--- tt/xmlschema.py
+++ tt/xmlschema.py
@@ -21,10 +21,24 @@ with columns
 
     (element name) (simple or complex) (mixed or pure content)
 
+TEI mode
+--------
+Analyses the TEI schema.
+If you pass an optional customized TEI schema, it will be passed separately,
+and the resulting info will be merged with the analysis result of the
+complete TEI schema.
+The information from the customised schema overrides the information from the
+complete TEI schema.
+The complete TEI schema is part of this package, you do not have to provide it.
+It has been generated on with the online TEI-Roma tool at
+https://roma.tei-c.org/startroma.php.
+
+
 USAGE
 
 Command line:
 
+xmlschema tei [schemafile.xsd]
 xmlschema analyse {schemafile.xsd}
 xmlschema fromrelax {schemafile.rng}
 
@@ -38,23 +52,6 @@ It could very well be that I have missed parts of the semantics of XML-Schema.
 
 
 class Analysis:
-    """Extracts meaningful information from an XML Schema.
-
-    When parsing XML it is sometimes needed to know the properties of the current
-    element, especially whether it allows mixed content or not.
-
-    If it does not, it is safe to discard white-space, otherwise not.
-
-    Moreover, if there are two adjacent elements, each containing text,
-    are the string at the end of the first element and the string at the start of the
-    second element part of the same word?
-
-    If both elements are contained in a element that does not allow mixed content,
-    they are separate words (the XML-elements are used as data containers);
-    otherwise they belong to the same word (the XML-elements annotate a piece
-    of string).
-    """
-
     types = set(
         """
         simpleType
@@ -70,17 +67,58 @@ class Analysis:
         """.strip().split()
     )
 
-    def __init__(self, schemaPath, debug=False):
+    def __init__(self, baseSchema, override=None, debug=False):
+        """Extracts meaningful information from an XML Schema.
+
+        When parsing XML it is sometimes needed to know the properties of the current
+        element, especially whether it allows mixed content or not.
+
+        If it does not, it is safe to discard white-space, otherwise not.
+
+        Moreover, if there are two adjacent elements, each containing text,
+        are the string at the end of the first element and the string at the start of the
+        second element part of the same word?
+
+        If both elements are contained in a element that does not allow mixed content,
+        they are separate words (the XML-elements are used as data containers);
+        otherwise they belong to the same word (the XML-elements annotate a piece
+        of string).
+
+        Parameters
+        ----------
+        baseSchema: string
+            The path of the xsd file that acts as the base schema that we want
+            to analyse.
+        override: string, optional None
+            The path of another schema intended to override parts of the baseSchema.
+        debug: boolean, optional False
+            Whether to run in debug mode or not.
+            In debug mode more information is printed on the console.
+        """
+
         self.debug = debug
 
         try:
-            with open(schemaPath) as fh:
+            with open(baseSchema) as fh:
                 tree = etree.parse(fh)
 
             self.root = tree.getroot()
+
+            self.oroot = None
+
+            if override is not None:
+                with open(baseSchema) as fh:
+                    otree = etree.parse(fh)
+
+                self.oroot = otree.getroot()
+
             self.good = True
+
         except Exception as e:
-            print(f"Could not read and parse {schemaPath}")
+            msg = f"Could not read and parse {baseSchema}"
+            if override is not None:
+                msg += " or {override}"
+            print(msg)
             print(str(e))
             self.good = False
 
@@ -209,25 +247,64 @@ class Analysis:
                 findDefs(child, defining, top)
 
         findDefs(root, False, False)
-        self.definitions = definitions
-        self.redefinitions = redefinitions
         if debug:
             self.printElems()
+        self.resolve(definitions)
 
-        self.resolve()
+        baseDefinitions = definitions
+
+        oroot = self.oroot
+
+        def repMixed(m):
+            return "-----" if m is None else "mixed" if m else "pure"
+
+        def repKind(k):
+            return "-----" if k is None else k
+
+        self.overrides = {}
+
+        if oroot is not None:
+            definitions = {}
+            redefinitions = collections.Counter()
+            findDefs(oroot, False, False)
+            if debug:
+                self.printElems()
+            self.resolve(definitions)
+
+            for (name, odef) in definitions.items():
+                if name in definitions:
+                    baseDef = baseDefinitions[name]
+                    baseKind = repKind(baseDef.get("kind", None))
+                    baseMixed = repMixed(baseDef.get("mixed", None))
+                    oKind = repKind(odef.get("kind", None))
+                    oMixed = repMixed(odef.get("mixed", None))
+                    transRep = (
+                        f"{baseKind} {baseMixed} ==> {oKind} {oMixed}"
+                        if baseKind != oKind and baseMixed != oMixed
+                        else f"{baseKind} ==> {oKind}"
+                        if baseKind != oKind
+                        else f"{baseMixed} ==> {oMixed}"
+                        if baseMixed != oMixed
+                        else None
+                    )
+                    if transRep is not None:
+                        baseDefinitions[name] = odef
+                    self.overrides[name] = transRep
         defs = tuple(
             (name, info.get("kind", None), info.get("mixed", None))
-            for (name, info) in sorted(definitions.items(), key=self.eKey)
+            for (name, info) in sorted(baseDefinitions.items(), key=self.eKey)
             if info["tag"] == "element" and not info["abstract"]
         )
-        if asTsv:
-            return "\n".join(
-                f"""{name}\t{kind or "---"}\t{"mixed" if mixed else "pure"}"""
+        return (
+            "\n".join(
+                f"{name}\t{repKind(kind)}\t{repMixed(mixed)}"
                 for (name, kind, mixed) in defs
             )
-        return defs
+            if asTsv
+            else defs
+        )
 
-    def resolve(self):
+    def resolve(self, definitions):
         """Resolve indirections in the definitions.
 
         After having read the complete XSD file,
@@ -235,7 +312,6 @@ class Analysis:
         in places where the names occur.
         """
         debug = self.debug
-        definitions = self.definitions
 
         def infer():
             changed = 0
@@ -245,14 +321,26 @@ class Analysis:
 
                 other = info.get("base", info.get("subs", None))
                 if other:
-                    other = other.split(":", 1)[-1]
-                    otherInfo = definitions[other]
+                    otherBare = other.split(":", 1)[-1]
+                    otherInfo = definitions.get(otherBare, None)
+                    if otherInfo is None:
+                        print(f"Warning: {other} is not defined.")
+                        continue
                     if otherInfo["mixed"]:
                         info["mixed"] = True
                         changed += 1
-                    if info.get("kind", None) is None and otherInfo.get("kind", None):
-                        info["kind"] = otherInfo["kind"]
-                        changed += 1
+                    if info.get("kind", None) is None:
+                        if otherInfo.get("kind", None):
+                            info["kind"] = otherInfo["kind"]
+                            changed += 1
+                        else:
+                            print(f"Warning: {other}.kind is not defined.")
+                    if info.get("mixed", None) is None:
+                        if otherInfo.get("mixed", None):
+                            info["mixed"] = otherInfo["mixed"]
+                            changed += 1
+                        else:
+                            print(f"Warning: {other}.mixed is not defined.")
 
             return changed
 
@@ -274,6 +362,7 @@ class Analysis:
         Mainly for debugging.
         """
         definitions = self.definitions
+        redefinitions = self.redefinitions
 
         for (name, info) in sorted(definitions.items(), key=self.eKey):
             tag = info["tag"]
@@ -289,10 +378,16 @@ class Analysis:
                 f"({kind:<7}) ({mixed}) ({abstract}) {subsRep}{baseRep}"
             )
 
-        redefinitions = self.redefinitions
         print("=============================================")
         for (name, amount) in sorted(redefinitions.items()):
             print(f"{amount:>3}x {name}")
+
+
+TASKS = dict(
+    tei={0, 1},
+    analyse={1},
+    fromrelax={1},
+)
 
 
 def main():
@@ -301,28 +396,53 @@ def main():
         print(HELP)
         return 0
 
-    if len(args) != 2:
+    if len(args) == 0:
         print(HELP)
+        print("No task specified")
         return -1
 
-    (task, schemaFile) = args
-
-    if task not in {"fromrelax", "analyse"}:
+    task = args.pop(0)
+    nParams = TASKS.get(task, None)
+    if nParams is None:
         print(HELP)
-
-        print(f"Wrong task: `{task}`")
+        print(f"Unrecognized task {task}")
         return -1
 
-    if task == "analyse":
-        A = Analysis(schemaFile, debug=False)
+    if len(args) not in nParams:
+        print(HELP)
+        print(f"Wrong number of arguments ({len(args)} for {task}")
+        return -1
+
+    if task in {"tei", "fromrelax"}:
+        myDir = os.path.dirname(os.path.abspath(__file__))
+
+    if task in {"tei", "analyse"}:
+        if task == "tei":
+            baseSchema = f"{myDir}/tei/tei_all.xsd"
+            override = args[0] if len(args) else None
+        else:
+            baseSchema = args[0]
+            override = None
+
+        A = Analysis(baseSchema, override=override, debug=False)
         if not A.good:
             return 1
 
         defs = A.interpret(asTsv=True)
         print(defs)
+        print(f"!!!!!!!!!! {len(defs):>3} elements defined")
+        if override:
+            overrides = A.overrides
+            same = sum(1 for x in overrides.items() if x[1] is None)
+            distinct = len(overrides) - same
+            print(f"!!!!!!!!!! {same:>3} identical overrides")
+            print(f"!!!!!!!!!! {distinct:>3} changing overrides")
+        for (name, trans) in sorted(x for x in A.overrides.items() if x[1] is not None):
+            print(f"!!!!!!!!!! {name} {trans}")
         return 0
 
     if task == "fromrelax":
+        schemaFile = args[0]
         myDir = os.path.dirname(os.path.abspath(__file__))
         trang = f"{myDir}/trang/trang.jar"
         schemaOut = schemaFile.removesuffix(".rng") + ".xsd"
